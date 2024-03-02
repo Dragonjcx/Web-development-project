e909692172545b5c9edcbca11a98b1d4654499d3
Dragonjcx <156013740+Dragonjcx@users.noreply.github.com>
2024-03-02 16:51:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+document.addEventListener('DOMContentLoaded', function () 
+{
+const contactForm = document.getElementById('contact-form');
+if (contactForm)
+{
+contactForm.addEventListener('submit', function (event)
+{
+event.preventDefault(); // Prevent form submission
+const formData = new FormData(contactForm);
+for (const [key, value] of formData.entries())
+{
+console.log(`${key}: ${value}`);
+}
+});
+}
+});
